ec599e4eadc0dd0e795b5d4bb0eccd973e763f44
roobeen-b <rubinbaidhya@gmail.com>
2025-10-10 22:00:44 +0545

    Add ref guard and return statement to UserSync component

--- src/components/UserSync.tsx
+++ src/components/UserSync.tsx
@@ -1,25 +1,31 @@
 "use client";
 
-import { useEffect } from "react";
 import { useUser } from "@clerk/nextjs";
+import { useEffect, useRef } from "react";
 import { syncUserWithClerk } from "@/lib/actions/users";
 
 function UserSync() {
+  const hasCalledApi = useRef(false);
   const { isLoaded, isSignedIn } = useUser();
 
   useEffect(() => {
+    if (hasCalledApi.current) return;
     const handleUserSync = async () => {
       if (isLoaded && isSignedIn) {
         try {
           await syncUserWithClerk();
         } catch (error) {
           console.log("Error syncing user with Clerk", error);
+        } finally {
+          hasCalledApi.current = true;
         }
       }
     };
 
     handleUserSync();
   }, [isLoaded, isSignedIn]);
+
+  return null;
 }
 
 export default UserSync;
